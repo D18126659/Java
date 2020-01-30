8d2372d8ac145e52438ecf86a2c70646ffdb42d4
D18126659 <d18126659@mytudublin.ie>
2020-01-30 22:14:47 +0000

    programming lab 1 Signed-off-by: D18126659 <d18126659@mytudublin.ie>

---
+++ lab 1/src/Project.java
@@ -0,0 +1,12 @@
+/*****************************************
+ * Control: Make a basic program to excecute a java program
+ * Author: Sultan Alghamdi
+ * Date: 30th january 2020
+ */
+public class Project {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+
+}
