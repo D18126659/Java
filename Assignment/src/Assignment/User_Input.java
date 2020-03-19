8359482e441e6decc7cc47e293fecd4bebdb41ce
D18126659 <55544206+D18126659@users.noreply.github.com>
2020-03-19 22:55:06 +0000

    Add files via upload
    Updated User_inputs

--- Assignment/src/Assignment/User_Input.java
+++ Assignment/src/Assignment/User_Input.java
@@ -1,135 +1,162 @@
 package Assignment;
 
+import com.sun.tools.javac.Main;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Flow;
 
+public class User_Input extends Gui implements ActionListener{
 
-public class User_Input extends Gui implements ActionListener {
-    private JPanel application, buttonlayout;
-    private JLabel temp, ache, travel, cough, sore;
+    private JPanel mainPanel;
+    private JLabel title, temp, ache, travel, cough, sore;
     private JButton button;
+    private JRadioButton temperatureNormal, temperatureCool, temperatureHot, achesYes, achesNo, coughYes, coughNo, soreThroatYes,
+            soreThroatNo, RecentlytraveledYes, RecentlytraveledNo;
 
+    public User_Input()
+    {
+        panels();
+        titles();
+        temperature();
+        ache();
+        cough();
+        sore();
+        travel();
+        button();
+    }
 
+    public void panels()
+    {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(null);
+        add(mainPanel);
+    }
 
-    public User_Input() {
-
-
+    public void titles()
+    {
         setTitle("COVID-19");
-        application = new JPanel(new BorderLayout());
+        title = new JLabel("Please answer the following questions:");
+        title.setFont(new Font(null,Font.BOLD,14));
+        title.setBounds(60,0,300,50);
+        mainPanel.add(title);
+    }
 
-        buttonlayout=new JPanel(new FlowLayout());
-
+    public void temperature()
+    {
         temp = new JLabel("What is your temperature?");
-        JRadioButton temperatureNormal = new JRadioButton("Normal");
-        JRadioButton temperatureCool = new JRadioButton("Cool");
-        JRadioButton temperatureHot = new JRadioButton("Hot");
-        application.add(temp);
+        mainPanel.add(temp);
+
+        temperatureNormal = new JRadioButton("Normal");
+        temperatureCool = new JRadioButton("Cool");
+        temperatureHot = new JRadioButton("Hot");
 
         ButtonGroup temperature = new ButtonGroup();
         temperature.add(temperatureNormal);
         temperature.add(temperatureCool);
         temperature.add(temperatureHot);
-        application.add(temperatureNormal);
-        application.add(temperatureCool);
-        application.add(temperatureHot);
 
+        mainPanel.add(temperatureNormal);
+        mainPanel.add(temperatureCool);
+        mainPanel.add(temperatureHot);
 
+        temp.setBounds(115,60,150,20);
+        temperatureNormal.setBounds(150,90,150,20);
+        temperatureCool.setBounds(150,115,150,20);
+        temperatureHot.setBounds(150,140,150,20);
+    }
+
+    private void ache()
+    {
         ache = new JLabel("Have you been Aching a lot?");
-        JRadioButton achesYes = new JRadioButton("Yes");
-        JRadioButton achesNo = new JRadioButton("No");
-        application.add(ache);
+        achesYes = new JRadioButton("Yes");
+        achesNo = new JRadioButton("No");
+        mainPanel.add(ache);
 
         ButtonGroup aches = new ButtonGroup();
         aches.add(achesYes);
         aches.add(achesNo);
-        application.add(achesYes);
-        application.add(achesNo);
+        mainPanel.add(achesYes);
+        mainPanel.add(achesNo);
 
+        ache.setBounds(110,170,170,20);
+        achesYes.setBounds(150,200,170,20);
+        achesNo.setBounds(150,225,170,20);
+    }
+
+    public void cough()
+    {
         cough = new JLabel("Have you been coughing a lot?");
-        JRadioButton coughYes = new JRadioButton("Yes");
-        JRadioButton coughNo = new JRadioButton("No");
-        application.add(cough);
+        coughYes = new JRadioButton("Yes");
+        coughNo = new JRadioButton("No");
+        mainPanel.add(cough);
 
         ButtonGroup coughs = new ButtonGroup();
-        coughs.add(achesYes);
-        coughs.add(achesNo);
-        application.add(coughYes);
-        application.add(coughNo);
+        coughs.add(coughYes);
+        coughs.add(coughNo);
+        mainPanel.add(coughYes);
+        mainPanel.add(coughNo);
 
+        cough.setBounds(100,255,180,20);
+        coughYes.setBounds(150,285,170,20);
+        coughNo.setBounds(150,310,170,20);
+    }
 
+    public void sore()
+    {
         sore = new JLabel("Do you have a sore throat?");
-        JRadioButton soreThroatYes = new JRadioButton("Yes");
-        JRadioButton soreThroatNo = new JRadioButton("No");
-        application.add(sore);
+        soreThroatYes = new JRadioButton("Yes");
+        soreThroatNo = new JRadioButton("No");
+        mainPanel.add(sore);
 
         ButtonGroup sores = new ButtonGroup();
-        sores.add(achesYes);
-        sores.add(achesNo);
-        application.add(soreThroatYes);
-        application.add(soreThroatNo);
+        sores.add(soreThroatYes);
+        sores.add(soreThroatNo);
+        mainPanel.add(soreThroatYes);
+        mainPanel.add(soreThroatNo);
 
+        sore.setBounds(110,340,180,20);
+        soreThroatYes.setBounds(150,370,170,20);
+        soreThroatNo.setBounds(150,395,170,20);
+    }
 
+    public void travel()
+    {
         travel = new JLabel("Have you recently traveled?");
-        JRadioButton RecentlytraveledYes = new JRadioButton("Yes");
-        JRadioButton RecentlytraveledNo = new JRadioButton("No");
-        application.add(travel);
-
+        RecentlytraveledYes = new JRadioButton("Yes");
+        RecentlytraveledNo = new JRadioButton("No");
+        mainPanel.add(travel);
 
         ButtonGroup travels = new ButtonGroup();
-        travels.add(achesYes);
-        travels.add(achesNo);
-        application.add(RecentlytraveledYes);
-        application.add(RecentlytraveledNo);
+        travels.add(RecentlytraveledYes);
+        travels.add(RecentlytraveledNo);
+        mainPanel.add(RecentlytraveledYes);
+        mainPanel.add(RecentlytraveledNo);
 
+        travel.setBounds(110,425,180,20);
+        RecentlytraveledYes.setBounds(150,455,170,20);
+        RecentlytraveledNo.setBounds(150,480,170,20);
+    }
+
+    public void button()
+    {
         button = new JButton("Test");
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button.setSize(50,50);
-        buttonlayout.add(button);
         button.addActionListener(this);
-        add(application);
+        mainPanel.add(button);
 
-        application.setLayout(new BoxLayout(application, BoxLayout.Y_AXIS));
-        application.setBorder(BorderFactory.createTitledBorder("Corona Test"));
-
-        temp.setBorder(BorderFactory.createEmptyBorder(20, 75, 10, 0));
-        temperatureCool.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-        temperatureHot.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-        temperatureNormal.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-
-        ache.setBorder(BorderFactory.createEmptyBorder(20, 75, 10, 0));
-        achesYes.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-        achesNo.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-
-        cough.setBorder(BorderFactory.createEmptyBorder(20, 75, 10, 0));
-        coughYes.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-        coughNo.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-
-        sore.setBorder(BorderFactory.createEmptyBorder(20, 75, 10, 0));
-        soreThroatYes.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-        soreThroatNo.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-
-
-        travel.setBorder(BorderFactory.createEmptyBorder(20, 75, 10, 0));
-        RecentlytraveledYes.setBorder(BorderFactory.createEmptyBorder(0, 125, 0, 50));
-        RecentlytraveledNo.setBorder(BorderFactory.createEmptyBorder(0, 125, 10, 50));
-
-        application.add(buttonlayout);
+        button.setBounds(130,530,100,20);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        CoronaTest Test = new CoronaTest();
-        hide();
+        if(!(temperatureCool.isSelected() || temperatureHot.isSelected() || temperatureNormal.isSelected()) ||
+                !(achesYes.isSelected() || achesNo.isSelected()) || !(coughYes.isSelected() || coughNo.isSelected()) ||
+                !(soreThroatYes.isSelected() || soreThroatNo.isSelected()) || !(RecentlytraveledNo.isSelected() || RecentlytraveledYes.isSelected()))
+            JOptionPane.showMessageDialog(null, "Error - You have to select all valid buttons", "Error", JOptionPane.ERROR_MESSAGE);
+        else {
+            CoronaTest Test = new CoronaTest();
+            hide();
+        }
     }
-
 }
